88e64b468155d7a833bb7ce0db98e7f6b9077e5f
harshiiit00-ai <harshiiit00@gmail.com>
2026-06-03 14:28:49 +0530

    Day-3 is uploaded

---
+++ Day-3/code11.c
@@ -0,0 +1,14 @@
+//code11 GCD
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    printf("GCD is %d\n", a);
+    return 0;
+}
