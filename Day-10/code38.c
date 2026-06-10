541a404709f7d4d4d6cb95cc045e2387a6e54c0e
harshiiit00-ai <harshiiit00@gmail.com>
2026-06-10 23:47:47 +0530

    Day-10 is uploaded

---
+++ Day-10/code38.c
@@ -0,0 +1,17 @@
+//code38 reverse pyramid
+#include<stdio.h>
+int main(){
+    int i,j,space,rows;
+    printf("Enter number of rows:");
+    scanf("%d",&rows);
+    for(i=row;i>=1;i--){
+        for(space=1;space<=rows-i;space++){
+            printf(" ");
+        }
+        for(j=1;j<=(2*i-1);j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
